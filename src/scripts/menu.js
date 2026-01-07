99a1daa39f7b2b2fe8f6e09df7c3ee1c19347c23
Luis Jabalera <wichojabalera@gmail.com>
2026-01-06 17:04:41 -0700

    Improving menu toggle

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,15 +1,33 @@
-export default function menuToggle(nav, menu, link) {
+export default function menuToggle(nav, menu, links) {
   const $nav = document.querySelector(nav);
-  // const $links = document.querySelectorAll(link);
   const $menu = document.querySelector(menu);
 
-  $menu.addEventListener("click", (e) => {
-    $nav.classList.toggle("active");
+  const openMenu = () => {
+    $nav.classList.add("active");
+    $menu.setAttribute("aria-expanded", "true");
+    $menu.setAttribute("aria-label", "Close menu");
+  };
+
+  const closeMenu = () => {
+    $nav.classList.remove("active");
+    $menu.setAttribute("aria-expanded", "false");
+    $menu.setAttribute("aria-label", "Open menu");
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target.closest(menu)) {
+      $nav.classList.contains("active") ? closeMenu() : openMenu();
+    }
+
+    if (
+      (e.target.closest(links) && $nav.classList.contains("active")) ||
+      (!e.target.closest(menu) && !e.target.closest(nav) && $nav.classList.contains("active"))
+    ) {
+      closeMenu();
+    }
   });
 
   document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && $nav.classList.contains("active")) {
-      $menu.click();
-    }
+    if (e.key === "Escape" && $nav.classList.contains("active")) closeMenu();
   });
 }
